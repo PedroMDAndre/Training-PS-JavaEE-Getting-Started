11098e1153e979992ef53c7c4df23627f6147137
PedroMDAndre <pedro.mda@gmail.com>
2022-02-18 15:37:36 +0000

    1st commit

---
+++ src/main/java/pt/training/bookstore/model/Book.java
@@ -0,0 +1,110 @@
+package pt.training.bookstore.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Book {
+    // Attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(length = 200)
+    private String title;
+    @Column(length = 10000)
+    private String description;
+    private Float unitCost;
+    private String isbn;
+    @Column(name = "publication_date")
+    @Temporal(TemporalType.DATE)
+    private Date publicationDate;
+    @Column(name = "nr_of_pages")
+    private Integer nrOfPages;
+    @Column(name = "image_url")
+    private String imageUrl;
+    private Language language;
+
+    // Constructor
+    public Book() {
+    }
+
+    // Methods
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(Float unitCost) {
+        this.unitCost = unitCost;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Integer getNrOfPages() {
+        return nrOfPages;
+    }
+
+    public void setNrOfPages(Integer nrOfPages) {
+        this.nrOfPages = nrOfPages;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", unitCost=" + unitCost +
+                ", isbn='" + isbn + '\'' +
+                ", publicationDate=" + publicationDate +
+                ", nrOfPages=" + nrOfPages +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", language=" + language +
+                '}';
+    }
+}
