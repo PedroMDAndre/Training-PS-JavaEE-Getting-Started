bc30773665cafb4fe55c40de0c6f9b59cee8492d
PedroMDAndre <pedro.mda@gmail.com>
2022-03-08 22:50:09 +0000

    navigation

--- bookstore-front/src/app/app-routing.module.ts
+++ bookstore-front/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BookFormComponent } from './book-form/book-form.component';
+import { BookListComponent } from './book-list/book-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BookListComponent },
+  { path: 'book-list', component: BookListComponent },
+  { path: 'book-form', component: BookFormComponent },
+  { path: 'book-detail/:bookId', component: BookDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bookstore-front/src/app/book-detail/book-detail.component.ts
+++ bookstore-front/src/app/book-detail/book-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'bs-book-detail',
@@ -17,9 +18,14 @@ export class BookDetailComponent implements OnInit {
     language: "English"
   }
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  delete() {
+    // Invoke the HTTP DELETE
+    this.router.navigate(['/book-list'])
   }
 
 }
